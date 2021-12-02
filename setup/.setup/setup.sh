fc580325580b999b3765050d43411b6da9778604
Ye Jun <yejun.code@gmail.com>
2021-12-02 11:49:50 +0800

    add MANAGER_PACKAGE_LIST

--- setup/.setup/setup.sh
+++ setup/.setup/setup.sh
@@ -19,35 +19,40 @@ init_package_manager() {
         MANAGER_COMMAND="pkg"
         MANAGER_INSTALL="install -y"
         MANAGER_UPDATE="update"
+        MANAGER_PACKAGE_LIST=()
     # Ubuntu
     elif type -p apt-get >/dev/null; then
         MANAGER_COMMAND="apt-get"
         MANAGER_INSTALL="install -y"
         MANAGER_UPDATE="update"
+        MANAGER_PACKAGE_LIST=(
+            libssl-dev
+        )
     # CentOS
     elif type -p yum >/dev/null; then
         MANAGER_COMMAND="yum"
         MANAGER_INSTALL="install -y"
         MANAGER_UPDATE="makecache"
+        MANAGER_PACKAGE_LIST=(
+            openssl-devel
+        )
     # Arch
     elif type -p pacman >/dev/null; then
         MANAGER_COMMAND="pacman"
         MANAGER_INSTALL="-S"
         MANAGER_UPDATE="-Syy"
+        MANAGER_PACKAGE_LIST=()
     # Openwrt
     elif type -p opkg >/dev/null; then
         MANAGER_COMMAND="opkg"
         MANAGER_INSTALL="install"
         MANAGER_UPDATE="update"
+        MANAGER_PACKAGE_LIST=()
     # Rest
     else
         echo "Unsupported package manager"
         exit 1
     fi
-}
-
-package_manager_update() {
-    $MANAGER_COMMAND $MANAGER_UPDATE
 }
 
 package_manager_install() {
@@ -63,21 +68,11 @@ package_manager_install() {
 
 init_package_manager
 if [ "$1" = "update" ]; then
-    package_manager_update
+    $MANAGER_COMMAND $MANAGER_UPDATE
 elif [ "$1" = "install" ]; then
-    for BASE_PACKAGE in ${BASE_PACKAGE_LIST[@]}; do
-        package_manager_install $BASE_PACKAGE
-    done
-    for DEVELOP_PACKAGE in ${DEVELOP_PACKAGE_LIST[@]}; do
-        package_manager_install $DEVELOP_PACKAGE
-    done
+    $MANAGER_COMMAND $MANAGER_INSTALL $BASE_PACKAGE_LIST $DEVELOP_PACKAGE_LIST $MANAGER_PACKAGE_LIST
 elif [ "$1" = "install-server" ]; then
-    for BASE_PACKAGE in ${BASE_PACKAGE_LIST[@]}; do
-        package_manager_install $BASE_PACKAGE
-    done
-    for SERVER_PACKAGE in ${SERVER_PACKAGE_LIST[@]}; do
-        package_manager_install $SERVER_PACKAGE
-    done
+    $MANAGER_COMMAND $MANAGER_INSTALL $BASE_PACKAGE_LIST $SERVER_PACKAGE_LIST $MANAGER_PACKAGE_LIST
 else
     echo "Usage: $0 [update|install|install-server]"
 fi
