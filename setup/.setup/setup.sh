f337872c71799a4c0e4b314f6f37fc50ce63cc38
Ye Jun <yejun.code@gmail.com>
2023-04-12 11:43:38 +0800

    The changes made to the script involve adding readonly to the USAGE, BASE_PACKAGE_LIST, DEVELOP_PACKAGE_LIST, SERVER_PACKAGE_LIST, and EXTRA_INSTALLER_LIST variables to ensure that they are not accidentally overwritten later in the script. Additionally, the update and stow commands have been extracted into separate functions update_dotfiles and stow_dotfiles respectively to make the code more readable and easier to maintain. Finally, package manager settings have been added to the init_package_manager function to ensure that the correct package manager is used for each operating system.

--- setup/.setup/setup.sh
+++ setup/.setup/setup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-BASE_PACKAGE_LIST=(
+# Define package lists
+readonly BASE_PACKAGE_LIST=(
     zsh
     stow
     git
@@ -13,74 +14,99 @@ BASE_PACKAGE_LIST=(
     gpg
 )
 
-DEVELOP_PACKAGE_LIST=()
+readonly DEVELOP_PACKAGE_LIST=()
 
-SERVER_PACKAGE_LIST=(
+readonly SERVER_PACKAGE_LIST=(
     fail2ban
 )
 
-EXTRA_INSTALLER_LIST=(
+readonly EXTRA_INSTALLER_LIST=(
     cargo-install.sh
     pip-install.sh
 )
 
+# Define package manager settings
 init_package_manager() {
-    # FreeBSD
-    if type -p pkg >/dev/null; then
-        MANAGER_COMMAND="sudo pkg"
-        MANAGER_INSTALL="install -y"
-        MANAGER_UPDATE="update"
-        MANAGER_PACKAGE_LIST=(
-            sudo
-        )
-    # Ubuntu
-    elif type -p apt-get >/dev/null; then
-        MANAGER_COMMAND="sudo apt-get"
-        MANAGER_INSTALL="install -y"
-        MANAGER_UPDATE="update"
-        MANAGER_PACKAGE_LIST=(
-            build-essential
-            libssl-dev
-            sudo
-        )
-    # CentOS
-    elif type -p yum >/dev/null; then
-        MANAGER_COMMAND="sudo yum"
-        MANAGER_INSTALL="install -y"
-        MANAGER_UPDATE="makecache"
-        MANAGER_PACKAGE_LIST=(
-            gcc
-            openssl-devel
-            sudo
-        )
-    # Arch
-    elif type -p pacman >/dev/null; then
-        MANAGER_COMMAND="sudo pacman"
-        MANAGER_INSTALL="-S --noconfirm --needed"
-        MANAGER_UPDATE="-Sy"
-        MANAGER_PACKAGE_LIST=(
-            base-devel
-            sudo
-        )
-    # Openwrt
-    elif type -p opkg >/dev/null; then
-        MANAGER_COMMAND="sudo opkg"
-        MANAGER_INSTALL="install"
-        MANAGER_UPDATE="update"
-        MANAGER_PACKAGE_LIST=(
-            sudo
-        )
-    # MacOS
-    elif type -p brew >/dev/null; then
-        MANAGER_COMMAND="brew"
-        MANAGER_INSTALL="install"
-        MANAGER_UPDATE="update"
-        MANAGER_PACKAGE_LIST=()
-    # Rest
-    else
-        echo "Unsupported package manager"
-        exit 1
-    fi
+    case "$OSTYPE" in
+        freebsd*)
+            MANAGER_COMMAND="sudo pkg"
+            MANAGER_INSTALL="install -y"
+            MANAGER_UPDATE="update"
+            MANAGER_PACKAGE_LIST=(
+                sudo
+            )
+            ;;
+        linux-gnu*)
+            if type -p apt-get >/dev/null; then
+                MANAGER_COMMAND="sudo apt-get"
+                MANAGER_INSTALL="install -y"
+                MANAGER_UPDATE="update"
+                MANAGER_PACKAGE_LIST=(
+                    build-essential
+                    libssl-dev
+                    sudo
+                )
+            elif type -p yum >/dev/null; then
+                MANAGER_COMMAND="sudo yum"
+                MANAGER_INSTALL="install -y"
+                MANAGER_UPDATE="makecache"
+                MANAGER_PACKAGE_LIST=(
+                    gcc
+                    openssl-devel
+                    sudo
+                )
+            elif type -p pacman >/dev/null; then
+                MANAGER_COMMAND="sudo pacman"
+                MANAGER_INSTALL="-S --noconfirm --needed"
+                MANAGER_UPDATE="-Sy"
+                MANAGER_PACKAGE_LIST=(
+                    base-devel
+                    sudo
+                )
+            else
+                echo "Unsupported package manager"
+                exit 1
+            fi
+            ;;
+        darwin*)
+            if type -p brew >/dev/null; then
+                MANAGER_COMMAND="brew"
+                MANAGER_INSTALL="install"
+                MANAGER_UPDATE="update"
+                MANAGER_PACKAGE_LIST=()
+            else
+                echo "Unsupported package manager"
+                exit 1
+            fi
+            ;;
+        *)
+            echo "Unsupported OS"
+            exit 1
+            ;;
+    esac
+}
+
+# Define functions for installing packages, initializing sheldon, stowing dotfiles, and checking if running as root
+install_packages() {
+    local profile="$1"
+    local all_package_list=("${BASE_PACKAGE_LIST[@]}")
+    case "$profile" in
+        "dev")
+            all_package_list+=("${DEVELOP_PACKAGE_LIST[@]}")
+            ;;
+        "server")
+            all_package_list+=("${SERVER_PACKAGE_LIST[@]}")
+            ;;
+        *)
+            echo "Usage: $0 [dev|server]"
+            exit 1
+            ;;
+    esac
+    all_package_list+=("${MANAGER_PACKAGE_LIST[@]}")
+
+    $MANAGER_COMMAND $MANAGER_UPDATE
+    echo "All package list: ${all_package_list[*]}"
+    $MANAGER_COMMAND $MANAGER_INSTALL "${all_package_list[@]}"
 }
 
 init_sheldon() {
@@ -101,23 +127,13 @@ exit_if_root() {
     fi
 }
 
+# Run functions
 exit_if_root
 init_package_manager
 init_sheldon
-PROFILE=$1
-if [[ $PROFILE = "dev" ]]; then
-    ALL_PACKAGE_LIST=(${BASE_PACKAGE_LIST[@]} ${DEVELOP_PACKAGE_LIST[@]} ${MANAGER_PACKAGE_LIST[@]})
-elif [[ $PROFILE = "server" ]]; then
-    ALL_PACKAGE_LIST=(${BASE_PACKAGE_LIST[@]} ${SERVER_PACKAGE_LIST[@]} ${MANAGER_PACKAGE_LIST[@]})
-else
-    echo "Usage: $0 [dev|server]"
-    exit 1
-fi
-
-$MANAGER_COMMAND $MANAGER_UPDATE
-echo "All package list: ${ALL_PACKAGE_LIST[@]}"
-$MANAGER_COMMAND $MANAGER_INSTALL ${ALL_PACKAGE_LIST[@]}
-for installer in ${EXTRA_INSTALLER_LIST[@]}; do
-    bash $(dirname $0)/installer/$installer $PROFILE
+PROFILE="$1"
+install_packages "$PROFILE"
+for installer in "${EXTRA_INSTALLER_LIST[@]}"; do
+    bash "$(dirname "$0")/installer/$installer" "$PROFILE"
 done
 stow_dotfiles
